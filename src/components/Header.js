bc6ec6f3c9d81d08e5a8d9d160ab2fedf002c3e5
PCAW <wadealiou00@gmail.com>
2023-03-29 09:08:40 -0400

    Updated links

--- src/components/Header.js
+++ src/components/Header.js
@@ -95,8 +95,8 @@ const Header = () => {
           <nav>
             <HStack spacing={8}>
               {/* Add links to Projects and Contact me section */}
-              <a href="#projects" onClick={handleClick('projects')}>Projects</a>
-              <a href="#contact-me" onClick={handleClick('contactme')} >Contact Me</a>
+              <a href="/#projects" onClick={handleClick('projects')}>Projects</a>
+              <a href="/#contact-me" onClick={handleClick('contactme')} >Contact Me</a>
             </HStack>
           </nav>
         </HStack>
